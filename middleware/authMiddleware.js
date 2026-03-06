788d602ad4ffc66f980a15091d4c8bdec4d1cba3
AimanShariff-g-commits <1at24cg006@atria.edu>
2026-03-06 18:56:05 +0530

    Updated MongoDB connection

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,17 +2,26 @@ const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
   try {
-    const token = req.header("Authorization");
+    const authHeader = req.header("Authorization");
 
-    if (!token) {
+    if (!authHeader) {
       return res.status(401).json({ message: "Access Denied. No Token Provided." });
     }
 
+    const token = authHeader.split(" ")[1]; // remove "Bearer "
+
+    if (!token) {
+      return res.status(401).json({ message: "Invalid Token Format" });
+    }
+
     const verified = jwt.verify(token, process.env.JWT_SECRET);
+
     req.admin = verified;
 
     next();
+
   } catch (error) {
+    console.error(error);
     res.status(400).json({ message: "Invalid Token" });
   }
 };
